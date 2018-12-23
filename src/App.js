20df28651ca14a39ff4a07c7f48f6dd7aae85d92
Philip Fulgham <philfulgham@gmail.com>
2018-12-22 23:52:43 -0500

    O green fir tree, gives us delight

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { Ornament } from "./components/Ornament";
+import { TreeSection } from "./components/TreeSection";
+import { TreeTrunk } from "./components/TreeTrunk";
+
+const XmasTree = styled.div`
+	align-items: center;
+	border: 2px solid black;
+	display: flex;
+	flex-direction: column;
+	height: 600px;
+	justify-content: flex-end;
+	margin: 40px;
+	position: relative;
+	width: 400px;
+`;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<XmasTree>
+				<TreeSection className="top" />
+				<TreeSection className="middle" />
+				<TreeSection />
+				<TreeTrunk />
+				<Ornament data-id="1" />
+				<Ornament data-id="2" />
+				<Ornament data-id="3" />
+				<Ornament data-id="4" />
+				<Ornament data-id="5" />
+				<Ornament data-id="6" />
+				<Ornament data-id="7" />
+				<Ornament data-id="8" />
+				<Ornament data-id="9" />
+			</XmasTree>
+		);
+	}
 }
 
 export default App;
