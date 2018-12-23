93492df6c7c8bb09e1b48bc616a39f1538cc096f
Philip Fulgham <philfulgham@gmail.com>
2018-12-22 23:52:13 -0500

    How sturdy God hath made thee

---
+++ src/components/TreeTrunk.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+export const TreeTrunk = styled.div`
+	background-color: saddlebrown;
+	height: 75px;
+	width: 100px;
+`;
